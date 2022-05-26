456b4dedf80e3e94f6ef5870a32cb1c51ec463f7
Valeriia Shastun <pariotti333@gmail.com>
2022-05-26 23:42:34 +0200

    homework#10 for lesson08 refactoring

--- lesson08/hw10_lesson08.py
+++ lesson08/hw10_lesson08.py
@@ -1,18 +1,31 @@
 from bs4 import BeautifulSoup
 import requests
 
+class GetPagesCount:
 
-url = "https://quotes.toscrape.com/"
-req = requests.get(url)
+    def __init__(self, url):
+        self.url = url
 
-soup = BeautifulSoup(req.content, 'html.parser')
-tag_with_next_page = soup.find("li", class_="next")
-count = 1
-while tag_with_next_page is not None:
-    link_to_next_page = "https://quotes.toscrape.com" + tag_with_next_page.find("a").get("href")
-    next_page_navigate = requests.get(link_to_next_page)
-    soup = BeautifulSoup(next_page_navigate.content, 'html.parser')
-    tag_with_next_page = soup.find("li", class_="next")
-    count += 1
+    def get_pages_count(self, url):
+        """
+        Will give quantity of pages which are available in the web page
+        :param url: url of web page
+        :return: pages count
+        """
+        req = requests.get(url)
+        soup = BeautifulSoup(req.content, 'html.parser')
+        tag_with_next_page = soup.find("li", class_="next")
+        count = 1
+        while tag_with_next_page is not None:
+            link_to_next_page = "https://quotes.toscrape.com" + tag_with_next_page.find("a").get("href")
+            next_page_navigate = requests.get(link_to_next_page)
+            soup = BeautifulSoup(next_page_navigate.content, 'html.parser')
+            tag_with_next_page = soup.find("li", class_="next")
+            count += 1
+        return count
 
-print(f"Total pages amount on website is {count}")
+
+if __name__ == "__main__":
+    url = "https://quotes.toscrape.com/"
+    pages_count = GetPagesCount(url).get_pages_count(url)
+    print(f"Total pages amount on website is {pages_count}")
