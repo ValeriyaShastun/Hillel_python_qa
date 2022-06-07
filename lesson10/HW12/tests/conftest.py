505a1d845365775e9f2a4b636cb933b54ab6084b
Valeriia Shastun <pariotti333@gmail.com>
2022-06-07 21:14:47 +0200

    refactored HW12, lesson 10

--- lesson10/HW12/tests/conftest.py
+++ lesson10/HW12/tests/conftest.py
@@ -5,14 +5,28 @@ from lesson10.HW12.code_for_testing import Human
 logger = logging.getLogger()
 logger.setLevel('INFO')
 
+
 # Write test cases for the provided file using pytest. Implement fixtures for your tests using yield or return,
 # group tests using pytest marks, log fixture start and fixture end. Try parametrization for one TC.
 
 @pytest.fixture()
 def create_human():
     logger.info(msg='\nFixture "create_human" start')
+
     def human_factory(name, age, gender):
         return Human(name=name, age=age, gender=gender)
 
     yield human_factory
     logger.info(msg='\nFixture "create_human" finished')
+
+
+@pytest.fixture()
+def generate_data_for_human():
+    logger.info(msg='\nFixture "generate_data_for_human" start')
+
+    def data_factory(name, age, gender):
+        dict_human_data = {"name": name, "age": age, "gender": gender}
+        return dict_human_data
+
+    yield data_factory
+    logger.info(msg='\nFixture "generate_data_for_human" finished')
--- lesson10/HW12/tests/test_human_class.py
+++ lesson10/HW12/tests/test_human_class.py
@@ -6,7 +6,7 @@ logger.setLevel('INFO')
 
 
 @pytest.mark.regression
-def test_human_check_status_alive(create_human):
+def test_human_status_alive(create_human, generate_data_for_human):
     """
     Description: test checks whether Human is alive by calling Human "status" property.
     When alive returns "alive", else: AssertionError will be raised.
@@ -20,16 +20,14 @@ def test_human_check_status_alive(create_human):
     Expected:
     1. Receive 'alive' when checking Human Anna "status" property
     """
-    name_Anna = 'Anna'
-    age_Anna = 22
-    gender_Anna = 'female'
-    human_alive = create_human(name=name_Anna, age=age_Anna, gender=gender_Anna)
+    dict_data = generate_data_for_human('Anna', 22, 'female')
+    human_alive = create_human(name=dict_data.get("name"), age=dict_data.get("age"), gender=dict_data.get("gender"))
     assert human_alive.status == "alive", f"\n{human_alive.status} status is not as expected " \
                                           f"\nActual: {human_alive.status} \nExpected: 'alive' "
 
 
 @pytest.mark.regression
-def test_check_human_age(create_human):
+def test_human_age(create_human, generate_data_for_human):
     """
     Description: test checks Human age by creating instance of Human class and
     calling Human "age" property, checking that the age is the same as passed into
@@ -42,19 +40,17 @@ def test_check_human_age(create_human):
     1. Check that Human Liza is 23 y.o.
 
     Expected:
-    1. Receive 22 when checking Human Anna "age" property
+    1. Receive 22 when checking Human Liza "age" property
     """
-    name_Liza = 'Liza'
-    age_Liza = 23
-    gender_Liza = 'female'
-    human_age_check = create_human(name=name_Liza, age=age_Liza, gender=gender_Liza)
-    assert human_age_check.age == age_Liza, f"\n{human_age_check.age} age is not as expected " \
-                                            f"\nActual: {human_age_check.age} \nExpected: {age_Liza} "
+    dict_data = generate_data_for_human('Liza', 23, 'female')
+    human_age_check = create_human(name=dict_data.get("name"), age=dict_data.get("age"), gender=dict_data.get("gender"))
+    assert human_age_check.age == dict_data.get("age"), f'\n{human_age_check.age} age is not as expected " \
+                                            f"\nActual: {human_age_check.age} \nExpected: {dict_data.get("age")} '
 
 
 @pytest.mark.regression
-@pytest.mark.parametrize("name, age, expected", [["Petro", 22, 23], ["Zhenya", 104, 105]])
-def test_check_human_grow(create_human, name, age, expected):
+@pytest.mark.parametrize("name, age, age_expected", [["Petro", 22, 23], ["Zhenya", 104, 105]])
+def test_human_grow(create_human, name, age, age_expected):
     """
     Description: test checks Human age after calling "grow" method. Creates instance of Human class and
     calling Human "grow" method, checking that the age has increased for 1 year.
@@ -72,15 +68,15 @@ def test_check_human_grow(create_human, name, age, expected):
     logger.info(f"\n TEST START for user {name}")
     name_user = name
     age_user = age
-    gender_Petro = 'male'
-    human_grow_check = create_human(name=name_user, age=age_user, gender=gender_Petro)
+    gender = 'male'
+    human_grow_check = create_human(name=name_user, age=age_user, gender=gender)
     human_grow_check.grow()
-    assert human_grow_check.age == expected, f"\n{human_grow_check.age} age is not as expected " \
-                                             f"\nActual: {human_grow_check.age} \nExpected: {expected} "
+    assert human_grow_check.age == age_expected, f"\n{human_grow_check.age} age is not as expected " \
+                                                 f"\nActual: {human_grow_check.age} \nExpected: {age_expected} "
 
 
 @pytest.mark.regression
-def test_check_dead_human_grow(create_human):
+def test_dead_human_grow(create_human, generate_data_for_human):
     """
     Description: test checks change of status from "alive" to "dead" for Human with age more than 105 y.o
     age after calling "grow" method. Human age does not increase.
@@ -95,20 +91,19 @@ def test_check_dead_human_grow(create_human):
     Expected:
     1. While checking "age" increased for 1 we expect Exception and while checking "status" we expect "dead"
     """
-    name_Lana = 'Lana'
-    age_Lana = 106
-    gender_Lana = 'female'
-    human_dead_check = create_human(name=name_Lana, age=age_Lana, gender=gender_Lana)
+    dict_data = generate_data_for_human('Lana', 106, 'female')
+    human_dead_check = create_human(name=dict_data.get("name"), age=dict_data.get("age"),
+                                    gender=dict_data.get("gender"))
     human_dead_check.grow()
-    assert human_dead_check.age != age_Lana + 1, f"\n{human_dead_check.age} age is not as expected " \
-                                                 f"\nActual: {human_dead_check.age} \nExpected: {age_Lana} "
+    assert human_dead_check.age != dict_data.get("age") + 1, f"\n{human_dead_check.age} age is not as expected " \
+                                                             f"\nActual: {human_dead_check.age} \nExpected: {dict_data.get('age')}"
     assert human_dead_check.status == "dead", f"\n{human_dead_check.status} status is not as expected " \
                                               f"\nActual: {human_dead_check.status} \nExpected: 'dead' "
 
 
 @pytest.mark.regression
 @pytest.mark.skip("Test is skipped unless bug with possibility of creation dead human is fixed")
-def test_attribute_status_for_dead_human(create_human):
+def test_attribute_status_for_dead_human(create_human, generate_data_for_human):
     """
    Description: test checks whether it is possible to create dead Human (with age gt 105)
 
@@ -121,16 +116,15 @@ def test_attribute_status_for_dead_human(create_human):
    Expected:
    1. While checking "status" we expect "dead"
    """
-    name_Lana = 'Lana'
-    age_Lana = 110
-    gender_Lana = 'female'
-    human_dead_check = create_human(name=name_Lana, age=age_Lana, gender=gender_Lana)
+    dict_data = generate_data_for_human('Lana', 110, 'female')
+    human_dead_check = create_human(name=dict_data.get("name"), age=dict_data.get("age"),
+                                    gender=dict_data.get("gender"))
     assert human_dead_check.status == "dead", f"\n{human_dead_check.status} status is not as expected " \
                                               f"\nActual: {human_dead_check.status} \nExpected: 'dead' "
 
 
 @pytest.mark.regression
-def test_check_human_name(create_human):
+def test_human_name(create_human, generate_data_for_human):
     """
     Description: test checks Human name by creating instance of Human class and
     calling Human "name" property, checking that the name is the same as passed into
@@ -145,16 +139,15 @@ def test_check_human_name(create_human):
     Expected:
     1. Receive Olga when checking Human "name" property
     """
-    name_Olga = 'Olga'
-    age_Olga = 34
-    gender_Olga = 'female'
-    human_name_check = create_human(name=name_Olga, age=age_Olga, gender=gender_Olga)
-    assert human_name_check.name == name_Olga, f"\n{human_name_check.name} name is not as expected " \
-                                               f"\nActual: {human_name_check.name} \nExpected: {name_Olga} "
+    dict_data = generate_data_for_human('Olga', 34, 'female')
+    human_name_check = create_human(name=dict_data.get("name"), age=dict_data.get("age"),
+                                    gender=dict_data.get("gender"))
+    assert human_name_check.name == dict_data.get("name"), f"\n{human_name_check.name} name is not as expected " \
+                                                           f"\nActual: {human_name_check.name} \nExpected: {dict_data.get('name')}"
 
 
 @pytest.mark.regression
-def test_check_change_name_positive(create_human):
+def test_change_name_positive(create_human, generate_data_for_human):
     """
     Description: test checks setting of new name by method "change_name".
 
@@ -168,10 +161,9 @@ def test_check_change_name_positive(create_human):
     Expected:
     1. After calling method "change_name" when calling property "name" receive name Kira
     """
-    name_Lama = 'Lama'
-    age_Lama = 106
-    gender_Lama = 'female'
-    human_name_change = create_human(name=name_Lama, age=age_Lama, gender=gender_Lama)
+    dict_data = generate_data_for_human('Lama', 106, 'female')
+    human_name_change = create_human(name=dict_data.get("name"), age=dict_data.get("age"),
+                                     gender=dict_data.get("gender"))
     new_name = "Kira"
     human_name_change.change_name(new_name)
     assert human_name_change.name == new_name, f"\n{human_name_change.name} age is not as expected " \
@@ -180,7 +172,7 @@ def test_check_change_name_positive(create_human):
 
 @pytest.mark.negative
 @pytest.mark.regression
-def test_check_change_name_negative(create_human):
+def test_change_name_negative(create_human, generate_data_for_human):
     """
     Description: test checks impossibility of setting of new name in lowercase and less than 10 letters
     by method "change_name".
@@ -195,20 +187,20 @@ def test_check_change_name_negative(create_human):
     Expected:
     1. After calling method "change_name" with parameter in lowercase/with length lt 10 letters we receive an exception
     """
-    name = "Lama"
-    age_user_neg = 88
-    gender_user_neg = 'female'
-    human_name_change = create_human(name=name, age=age_user_neg, gender=gender_user_neg)
+    dict_data = generate_data_for_human('Lama', 88, 'female')
+    human_name_change = create_human(name=dict_data.get("name"), age=dict_data.get("age"),
+                                     gender=dict_data.get("gender"))
     new_name = "lama"
-    with pytest.raises(SyntaxError) as e:
+    with pytest.raises(SyntaxError) as e:  # with raises(SyntaxError, match='Name should starts with capital letter'):
         human_name_change.change_name(new_name)
-    # assert e.value == SyntaxError('Name should starts with capital letter') #fixme
+    assert 'Name should starts with capital letter' in str(e.value)
+    assert e.type == SyntaxError
 
 
 @pytest.mark.negative
 @pytest.mark.regression
 @pytest.mark.skip("Skipping test until fixed possibility of setting name in lowercase")
-def test_check_change_name_negative_length_gt_10(create_human):
+def test_change_name_negative_length_gt_10(create_human, generate_data_for_human):
     """
     Description: test checks impossibility of setting of new name in lowercase by method "change_name".
 
@@ -222,18 +214,19 @@ def test_check_change_name_negative_length_gt_10(create_human):
     Expected:
     1. After calling method "change_name" with parameter in lowercase we receive an exception
     """
-    name = "Bamboleyo"
-    age_user_neg = 88
-    gender_user_neg = 'female'
+    dict_data = generate_data_for_human('Bamboleyo', 88, 'female')
     new_name = "lamklajsuiopefbnm"
-    human_name_change = create_human(name=name, age=age_user_neg, gender=gender_user_neg)
+    human_name_change = create_human(name=dict_data.get("name"), age=dict_data.get("age"),
+                                     gender=dict_data.get("gender"))
     human_name_change.change_name(new_name)
-    with pytest.raises(Exception):
+    with pytest.raises(SyntaxError) as e:
         human_name_change.change_name(new_name)
+    assert 'Name should starts with capital letter' in str(e.value)
+    assert e.type == SyntaxError
 
 
 @pytest.mark.regression
-def test_check_human_gender(create_human):
+def test_human_gender(create_human, generate_data_for_human):
     """
     Description: test checks Human gender by creating instance of Human class and
     calling Human "gender" property, checking that the gender is the same as passed into
@@ -251,13 +244,13 @@ def test_check_human_gender(create_human):
     1. Receive "female" gender when passing the same parameter when creating Human entity.
      Receive "male" gender after using using gender.setter
     """
-    name_Liza = 'Liza'
-    age_Liza = 20
-    gender_Liza = 'female'
+    dict_data = generate_data_for_human('Liza', 20, 'female')
     gender_new = 'male'
-    human_gender_check = create_human(name=name_Liza, age=age_Liza, gender=gender_Liza)
-    assert human_gender_check.gender == gender_Liza, f"\n{human_gender_check.gender} gender is not as expected " \
-                                                     f"\nActual: {human_gender_check.gender} \nExpected: {gender_Liza} "
+    human_gender_check = create_human(name=dict_data.get("name"), age=dict_data.get("age"),
+                                      gender=dict_data.get("gender"))
+    assert human_gender_check.gender == dict_data.get("gender"), \
+        f"\n{human_gender_check.gender} gender is not as expected " \
+        f"\nActual: {human_gender_check.gender} \nExpected: {dict_data.get('gender')} "
     human_gender_check.gender = gender_new
     assert human_gender_check.gender == gender_new, f"\n{human_gender_check.gender} gender is not as expected " \
                                                     f"\nActual: {human_gender_check.gender} \nExpected: {gender_new} "
@@ -265,7 +258,7 @@ def test_check_human_gender(create_human):
 
 @pytest.mark.negative
 @pytest.mark.regression
-def test_check_human_gender_negative(create_human):
+def test_human_gender_negative(create_human, generate_data_for_human):
     """
     Description: test checks Human gender.setter by creating instance of Human class with
     allowed gender. Setting gender to incorrect by using gender.setter.
@@ -282,17 +275,18 @@ def test_check_human_gender_negative(create_human):
     1. Receive "female" gender when passing the same parameter when creating Human entity.
      Receive "male" gender after using using gender.setter
     """
-    name_Valya = "Valya"
-    age_Valya = 20
-    gender_Valya = 'female'
+    dict_data = generate_data_for_human('Valya', 20, 'female')
     gender_new = 'cat'
-    human_gender_check = create_human(name=name_Valya, age=age_Valya, gender=gender_Valya)
-    with pytest.raises(Exception):
+    human_gender_check = create_human(name=dict_data.get("name"), age=dict_data.get("age"),
+                                      gender=dict_data.get("gender"))
+    with pytest.raises(Exception) as e:
         human_gender_check.gender = gender_new
+    assert 'Gender is not as expected' in str(e.value)
+    assert e.type == Exception
 
 
 @pytest.mark.interaction
-def test_check_make_friend(create_human):
+def test_make_friend(create_human, generate_data_for_human):
     """
     Description: test checks Human make_friends and get_friends methods by creating instance of
     Human class, checking quantity of friends with get_friends. Creating friend and checking
@@ -311,22 +305,22 @@ def test_check_make_friend(create_human):
     1. Receive empty list when calling get_friends for freshly created Human, successfully amend
     friends attribute of Human after calling make_friends method
     """
-    name_Valya = "Valya"
-    age = 20
-    gender = 'female'
-    name_Sveta = "Sveta"
-    human_Valya = create_human(name=name_Valya, age=age, gender=gender)
-    human_friend = create_human(name=name_Sveta, age=age, gender=gender)
+    dict_data_human = generate_data_for_human('Valya', 20, 'female')
+    dict_data_friend = generate_data_for_human('Sveta', 33, 'female')
+    human_Valya = create_human(name=dict_data_human.get("name"), age=dict_data_human.get("age"),
+                               gender=dict_data_human.get("gender"))
+    human_friend = create_human(name=dict_data_friend.get("name"), age=dict_data_friend.get("age"),
+                                gender=dict_data_friend.get("gender"))
     assert human_Valya.friends == [], f"Freshly created Human with name {human_Valya.name} cannot have friends"
     human_Valya.make_friends(human_friend)
     assert human_Valya.friends == [human_friend], f"Human with name {human_Valya.name} should have friend with " \
                                                   f"name {human_friend.name}"
 
 
-@pytest.mark.skip("Skipped untill bug of adding a friend with age within 'dead' range is fixed")
+@pytest.mark.skip("Skipped until bug of adding a friend with age within 'dead' range is fixed")
 @pytest.mark.negative
 @pytest.mark.interaction
-def test_check_make_friend_negative(create_human):
+def test_make_friend_negative(create_human, generate_data_for_human):
     """
     Description: test checks Human make_friends raises Exception in case we pass as a parameter Human
     with age in range 'dead'
@@ -343,14 +337,16 @@ def test_check_make_friend_negative(create_human):
     Expected:
     1. Impossible to add as a friend Human with age in range "dead"
     """
-    name_Valya = "Valya"
-    age = 20
-    gender = 'female'
-    name_Katya = "Katya"
-    age_friend = 107
-    human_Valya = create_human(name=name_Valya, age=age, gender=gender)
-    human_friend = create_human(name=name_Katya, age=age_friend, gender=gender)
-    with pytest.raises(Exception):
+    dict_data_human = generate_data_for_human('Valya', 20, 'female')
+    dict_data_friend = generate_data_for_human('Katya', 107, 'female')
+    human_Valya = create_human(name=dict_data_human.get("name"), age=dict_data_human.get("age"),
+                               gender=dict_data_human.get("gender"))
+    human_friend = create_human(name=dict_data_friend.get("name"), age=dict_data_friend.get("age"),
+                                gender=dict_data_friend.get("gender"))
+    with pytest.raises(Exception) as e:
         human_Valya.make_friends(human_friend)
+    assert f'{human_friend.name} is already dead' in str(e.value)
+    assert e.type == Exception
+
     assert human_Valya.friends == [], f"Human with name {human_Valya.name} should not have friend as friend " \
                                       f"added was in range of 'dead'"
