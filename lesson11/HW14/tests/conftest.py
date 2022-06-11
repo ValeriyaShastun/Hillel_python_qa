dc1787ef33859e060f7222b6f7cc84998c367923
Valeriia Shastun <pariotti333@gmail.com>
2022-06-11 21:36:13 +0200

    HW14 for lesson 11

--- lesson11/HW14/tests/conftest.py
+++ lesson11/HW14/tests/conftest.py
@@ -1,43 +1,34 @@
 import logging
 import pytest
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.firefox.service import Service
-from webdriver_manager.firefox import GeckoDriverManager
-from selenium.webdriver.edge.service import Service
-from webdriver_manager.microsoft import EdgeChromiumDriverManager
 
-
-
+from selenium.webdriver import Chrome, Edge, Firefox
 
 logger = logging.getLogger()
 logger.setLevel('INFO')
 
 
+@pytest.fixture()
+def chrome_driver():
+    logger.info(msg='\nFixture "chrome_driver" start')
+    driver_chrome = Chrome("/usr/local/bin/chromedriver")
+    yield driver_chrome
+    driver_chrome.quit()
+    logger.info(msg='\nFixture "chrome_driver" finished')
+
 
 @pytest.fixture()
-def get_driver():
-    logger.info(msg='\nFixture "get_driver" start')
-    def driver_factory(driver_name: str):
-        """
-        Returns driver instance corresponding to passed parameter.
-        :param driver_name: driver name, possible options: "firefox", "chrome", "edge"
-        :return: driver instance
-        """
-        driver = None
-        options = ["firefox", "chrome", "edge"]
-        if driver_name == "chrome":
-            driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-        elif driver_name == "firefox":
-            driver = webdriver.Firefox(service=Service(GeckoDriverManager().install()))
-        elif driver_name == "edge":
-            driver = webdriver.Edge(service=Service(EdgeChromiumDriverManager().install()))
-        elif driver_name not in options:
-            logger.info("The driver parameter is incorrect, should be one of {0}, {1}, {2}".format(*options))
-        return driver
+def firefox_driver():
+    logger.info(msg='\nFixture "firefox_driver" start')
+    driver_firefox = Firefox("/usr/local/bin/")  # geckodriver
+    yield driver_firefox
+    driver_firefox.quit()
+    logger.info(msg='\nFixture "firefox_driver" finished')
 
-    yield driver_factory
-    # driver.quit() # FIXME
-    logger.info(msg='\nFixture "get_driver" finished')
 
+@pytest.fixture()
+def edge_driver():
+    logger.info(msg='\nFixture "edge_driver" start')
+    driver_edge = Edge("/usr/local/bin/msedgedriver")
+    yield driver_edge
+    driver_edge.quit()
+    logger.info(msg='\nFixture "edge_driver" finished')
--- lesson11/HW14/tests/test_login.py
+++ lesson11/HW14/tests/test_login.py
@@ -6,15 +6,22 @@ from selenium.webdriver.common.by import By
 logger = logging.getLogger()
 logger.setLevel('INFO')
 
+username = "valeriia_is_here"
+password = "valeriiatest"
+login_url = "http://www.testyou.in/Login.aspx?ReturnUrl=%2fStudent%2fStudentIndex.aspx"
+home_page_url = "http://www.testyou.in/Student/StudentIndex.aspx"
+username_selector = '[name$="Container$txtUserLogin"]'
+password_selector = '[name$="Container$txtPassword"]'
+login_button_selector = '[type="submit"][name$="btnLoginn"]'
+
 @pytest.mark.positive
-@pytest.mark.parametrize("browser", ["firefox", "chrome", "edge"])
-def test_login_success(get_driver, browser):
+def test_login_chrome_success(chrome_driver):
     """
-    Description: test opens browser, inserts login, password, presses button [Login],
+    Description: test opens chrome browser, inserts login, password, presses button [Login],
     checks that url has changed for homepage url
 
     Pre-conditions:
-    1. Open browser
+    1. Open chrome browser
 
     Steps:
     1. Insert correct Login
@@ -23,17 +30,72 @@ def test_login_success(get_driver, browser):
     4. Check that url has changed for homepage url
 
     Expected:
-    1. User logged in successfully and he is on the Home page
+    1. User logged in successfully using chrome browser and he is on the Home page
     """
-    driver = get_driver(browser)
-    username = "valeriia_is_here"
-    password = "valeriiatest"
-    login_url = "http://www.testyou.in/Login.aspx?ReturnUrl=%2fStudent%2fStudentIndex.aspx"
-    home_page_url = "http://www.testyou.in/Student/StudentIndex.aspx"
+    driver = chrome_driver
+    driver.maximize_window()
     driver.get(login_url)
     time.sleep(4)
-    driver.find_element(By.CSS_SELECTOR, '[name$="Container$txtUserLogin"]').send_keys(username)
-    driver.find_element(By.CSS_SELECTOR, '[name$="Container$txtPassword"]').send_keys(password)
-    driver.find_element(By.CSS_SELECTOR, '[type="submit"][name$="btnLoginn"]').click()
+    driver.find_element(By.CSS_SELECTOR, username_selector).send_keys(username)
+    driver.find_element(By.CSS_SELECTOR, password_selector).send_keys(password)
+    driver.find_element(By.CSS_SELECTOR, login_button_selector).click()
+    time.sleep(5)
+    assert driver.current_url == home_page_url
+
+
+@pytest.mark.positive
+def test_login_firefox_success(firefox_driver):
+    """
+    Description: test opens firefox browser, inserts login, password, presses button [Login],
+    checks that url has changed for homepage url
+
+    Pre-conditions:
+    1. Open firefox browser
+
+    Steps:
+    1. Insert correct Login
+    2. Insert correct password
+    3. Press [Login] button
+    4. Check that url has changed for homepage url
+
+    Expected:
+    1. User logged in successfully using firefox browser and he is on the Home page
+    """
+    driver = firefox_driver
+    driver.maximize_window()
+    driver.get(login_url)
+    time.sleep(4)
+    driver.find_element(By.CSS_SELECTOR, username_selector).send_keys(username)
+    driver.find_element(By.CSS_SELECTOR, password_selector).send_keys(password)
+    driver.find_element(By.CSS_SELECTOR, login_button_selector).click()
+    time.sleep(5)
+    assert driver.current_url == home_page_url
+
+
+@pytest.mark.positive
+def test_login_edge_success(edge_driver):
+    """
+    Description: test opens edge browser, inserts login, password, presses button [Login],
+    checks that url has changed for homepage url
+
+    Pre-conditions:
+    1. Open edge browser
+
+    Steps:
+    1. Insert correct Login
+    2. Insert correct password
+    3. Press [Login] button
+    4. Check that url has changed for homepage url
+
+    Expected:
+    1. User logged in successfully using edge browser and he is on the Home page
+    """
+    driver = edge_driver
+    driver.maximize_window()
+    driver.get(login_url)
+    time.sleep(4)
+    driver.find_element(By.CSS_SELECTOR, username_selector).send_keys(username)
+    driver.find_element(By.CSS_SELECTOR, password_selector).send_keys(password)
+    driver.find_element(By.CSS_SELECTOR, login_button_selector).click()
     time.sleep(5)
     assert driver.current_url == home_page_url
