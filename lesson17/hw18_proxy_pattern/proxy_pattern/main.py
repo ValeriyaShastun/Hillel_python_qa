77902877ec0f680c4544af9822fb3322551db71d
Valeriia Shastun <pariotti333@gmail.com>
2022-07-04 22:00:45 +0200

    refactoring of HW18 proxy pattern

--- lesson17/hw18_proxy_pattern/proxy_pattern/main.py
+++ lesson17/hw18_proxy_pattern/proxy_pattern/main.py
@@ -1,16 +1,16 @@
-from lesson17.hw18_proxy_pattern.proxy_pattern.proxy_txt_reader import TxtProxyReader
-from lesson17.hw18_proxy_pattern.proxy_pattern.proxy_txt_writer import TxtProxyWriter
+from lesson17.hw18_proxy_pattern.proxy_pattern.proxy_txt_writer import TxtProxyReaderWriter
 from lesson17.hw18_proxy_pattern.proxy_pattern.txt_reader import TxtReader
 from lesson17.hw18_proxy_pattern.proxy_pattern.txt_writer import TxtWriter
 
 
 txt_reader = TxtReader('users.txt')
-proxy_reader = TxtProxyReader(txt_reader)
 txt_writer = TxtWriter('users.txt')
-proxy_writer = TxtProxyWriter(txt_writer)
+proxy_reader_writer = TxtProxyReaderWriter(txt_writer, txt_reader)
 
-proxy_writer.write_file('\nhello teacher!')
-print(proxy_reader.read_file())
+proxy_reader_writer.write_file('\nhello teacher!')
+print(proxy_reader_writer.read_file())
 print('\n')
-proxy_writer.write_file('\nhello teacher!')
-print(proxy_reader.read_file())
+proxy_reader_writer.write_file('\nhello teacher_2!')
+print(proxy_reader_writer.read_file())
+print('\n')
+print(proxy_reader_writer.read_file())
--- lesson17/hw18_proxy_pattern/proxy_pattern/proxy_txt_reader_writer.py
+++ lesson17/hw18_proxy_pattern/proxy_pattern/proxy_txt_reader_writer.py
@@ -1,12 +1,19 @@
+from lesson17.hw18_proxy_pattern.proxy_pattern.abstract_writer import Writer
+from lesson17.hw18_proxy_pattern.proxy_pattern.txt_writer import TxtWriter
 from lesson17.hw18_proxy_pattern.proxy_pattern.abstract_reader import Reader
 from lesson17.hw18_proxy_pattern.proxy_pattern.txt_reader import TxtReader
 
 
-class TxtProxyReader(Reader):
-    def __init__(self, txt_reader: TxtReader):
+class TxtProxyReaderWriter(Writer, Reader):
+    def __init__(self, txt_writer: TxtWriter, txt_reader: TxtReader):
         self.__result = ''
         self.__is_actual = False
+        self.__writer = txt_writer
         self.__reader = txt_reader
+
+    def write_file(self, new_data):
+        self.__result = self.__writer.write_file(new_data)
+        self.__is_actual = False
 
     def read_file(self):
         if self.__is_actual:
@@ -18,3 +25,4 @@ class TxtProxyReader(Reader):
 
     def update_actual_status(self, status):
         self.__is_actual = status
+
